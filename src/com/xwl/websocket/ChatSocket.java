73ef7a590de9d68f96a279e9c67eeacd18c02b15
wulin.xu <wulin.xu@heweisoft.com>
2019-12-19 11:53:13 +0800

    Initial commit

---
+++ src/com/xwl/websocket/ChatSocket.java
@@ -0,0 +1,189 @@
+package com.xwl.websocket;
+
+import com.alibaba.fastjson.JSON;
+import com.xwl.util.MessageUtil;
+
+import javax.servlet.http.HttpSession;
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+// websocket请求的url：/websocket
+@ServerEndpoint(value = "/websocket", configurator = GetHttpSessionConfigurator.class)
+public class ChatSocket {
+
+    // websocket会话的session
+    private Session session;
+    // 登录用户的session（servlet中的session）
+    private HttpSession httpSession;
+
+    // 保存当前系统中登录的用户的HttpSession信息, 及对应的Endpoint实例信息
+    private static Map<HttpSession, ChatSocket> onlineUsers = new HashMap<HttpSession, ChatSocket>();
+    // 当前登录的人数
+    private static int onlineCount = 0;
+
+    /**
+     * 当开启一个新的会话时调用，该方法是客户端和服务端握手成功后调用的方法
+     * @param session
+     * @param config
+     */
+    @OnOpen
+    public void onOpen(Session session, EndpointConfig config) {
+        // 1. 记录webSocket的会话信息对象Session
+        this.session = session;
+
+        // 2. 获取当前登录用户HttpSession信息.
+        HttpSession httpSession = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
+        this.httpSession = httpSession;
+
+        System.out.println("当前登录用户 : " + httpSession.getAttribute("username") + ", Endpoint : " + hashCode());
+
+        // 3. 记录当前登录用户信息, 及对应的Endpoint实例
+        if (httpSession.getAttribute("username") != null) {
+            onlineUsers.put(httpSession, this);
+        }
+
+        // 4. 获取当前所有登录用户 --------> ITCAST,HEIMA,TOM...
+        String names = getNames();
+
+        // 5. 组装消息 ---> {"data":"HEIMA,Deng,ITCAST","toName":"","fromName":"","type":"user"}
+        String message = MessageUtil.getContent(MessageUtil.TYPE_USER, "", "", names);
+
+        // 6. 通过广播的形式发送消息
+        // session.getBasicRemote().sendText("");
+        broadcastAllUsers(message);
+
+        // 7. 记录当前用户登录数 .
+        incrCount();
+    }
+
+    // {"fromName":"Deng","toName":"HEIMA","content":"约会呀"}
+    @OnMessage
+    public void onMessage(String message, Session session) throws IOException {
+        System.out.println("onMessage : name = " + httpSession.getAttribute("username") + ", message=" + message);
+        // 1. 获取客户端的信息内容, 并解析
+        Map<String, String> messageMap = JSON.parseObject(message, Map.class);
+        String fromName = messageMap.get("fromName");
+        String toName = messageMap.get("toName");
+        String content = messageMap.get("content");
+
+        // 2. 判定是否有接收人
+        if (toName == null || toName.isEmpty()) {
+            return;
+        }
+
+        // 3. 如果接收人是否是广播(all), 如果是, 则说明发送广播消息
+        String messageContent = MessageUtil.getContent(MessageUtil.TYPE_MESSAGE, fromName, toName, content);
+        System.out.println("服务端给客户端发送消息, 消息内容: " + messageContent);
+        if ("all".equals(toName)) {
+            // 3.1 组装消息内容
+            broadcastAllUsers(messageContent);
+        } else { // 4. 不是all , 则给指定的用户推送消息
+            singlePushMessage(messageContent, fromName, toName);
+        }
+    }
+
+    /**
+     * 给指定用户推送消息
+     * @param content
+     * @param fromName
+     * @param toName
+     * @throws IOException
+     */
+    private void singlePushMessage(String content, String fromName, String toName) throws IOException {
+        boolean isOnline = false;
+        // 1. 判定当然接收人是否在线
+        for (HttpSession hsession : onlineUsers.keySet()) {
+            if (toName.equals(hsession.getAttribute("username"))) {
+                isOnline = true;
+            }
+        }
+        // 2. 如果存在, 发送消息
+        if (isOnline) {
+            for (HttpSession hsession : onlineUsers.keySet()) {
+                if (hsession.getAttribute("username").equals(fromName) || hsession.getAttribute("username").equals(toName)) {
+                    onlineUsers.get(hsession).session.getBasicRemote().sendText(content);
+                }
+            }
+        }
+    }
+
+    /**
+     * 发送广播消息
+     * @param message
+     */
+    private void broadcastAllUsers(String message) {
+        for (HttpSession hsession : onlineUsers.keySet()) {
+            try {
+                onlineUsers.get(hsession).session.getBasicRemote().sendText(message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 获取所有的在线用户
+     * @return
+     */
+    private String getNames() {
+        String names = "";
+        if (onlineUsers.size() > 0) {
+            for (HttpSession hsession : onlineUsers.keySet()) {
+                String username = (String) hsession.getAttribute("username");
+                names += username + ",";
+            }
+        }
+        return names.substring(0, names.length() - 1);
+    }
+
+    /**
+     * 当会话关闭时调用
+     * @param session
+     * @param closeReason
+     */
+    @OnClose
+    public void onClose(Session session, CloseReason closeReason) {
+        decrCount();
+        System.out.println("客户端关闭了一个连接 , 当前在线人数 : " + getOnlineCount());
+    }
+
+    /**
+     * 当连接过程中异常时调用
+     * @param session
+     * @param throwable
+     */
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        throwable.printStackTrace();
+        System.out.println("服务异常");
+    }
+
+
+    /**
+     * 获取在线人数
+     * @return
+     */
+    public int getOnlineCount() {
+        return onlineCount;
+    }
+
+    /**
+     * 在线人数加一
+     * onlineCount++ 不是一个原子操作，存在并发问题，需要使用 synchronized 关键字解决
+     */
+    public synchronized void incrCount() {
+        onlineCount++;
+    }
+
+    /**
+     * 在线人数减一
+     * onlineCount-- 不是一个原子操作，存在并发问题，需要使用 synchronized 关键字解决
+     */
+    public synchronized void decrCount() {
+        onlineCount--;
+    }
+
+}
